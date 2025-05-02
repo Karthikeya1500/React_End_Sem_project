7f9793f68ce5370471543c19f4dc8cc616c5e969
Karthikeya1500 <karukarthikeya1111@gmail.com>
2025-05-02 11:10:20 +0530

    modified

--- Newsapp/vite.config.js
+++ Newsapp/vite.config.js
@@ -5,6 +5,18 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    cors: true
+    cors: true,
+    proxy: {
+      '/api': {
+        target: 'https://newsapi.org',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        configure: (proxy, options) => {
+          proxy.on('proxyReq', (proxyReq, req, res) => {
+            proxyReq.setHeader('X-Api-Key', '43c7bd4efad143be9de9f35313e3c3be');
+          });
+        }
+      }
+    }
   }
 })
